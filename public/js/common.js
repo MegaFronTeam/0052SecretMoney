d49cc503e63ac04f148a604e128149c45c8c73d4
Janis <janis.paberzs18@gmail.com>
2023-12-20 17:39:43 +0200

    feat: done 01 and 02 pages & start chart js

--- public/js/common.js
+++ public/js/common.js
@@ -114,6 +114,56 @@ function eventHandler() {
 		});
 	}
 
+	new hcSticky('.sDeal__deal-sticky--js', {
+    stickTo: '.sDeal',
+		mobileFirst: true,
+		top: 16,
+		responsive: {
+			998: {
+				disable: true
+			}
+		}
+  });
+
+	let chartDoughnutJsArr = document.querySelectorAll('.chart-doughnut-js');
+
+	if (chartDoughnutJsArr.length > 0) {
+		chartDoughnutJsArr.forEach((chartDoughnutJs) => {
+			const data = {
+				labels: [
+					'Red',
+					'Blue',
+					'Yellow'
+				],
+				datasets: [{
+					label: 'My First Dataset',
+					data: [300, 50, 100],
+					backgroundColor: [
+						'rgb(255, 99, 132)',
+						'rgb(54, 162, 235)',
+						'rgb(255, 205, 86)'
+					],
+					hoverOffset: 4,
+				}],
+			};
+			
+			new Chart(chartDoughnutJs, {
+				type: 'doughnut',
+				data: data,
+				options: {
+					responsive: true,
+					borderWidth: 0,
+					radius: '202',
+					plugins: {
+						legend: {
+							position: 'right',
+						},
+					}
+				},
+			});
+		})
+	}
+		
 };
 if (document.readyState !== 'loading') {
 	eventHandler();
